bd1a7fe6a2e27ca0c7f89e5b8e4faabc89941b38
coderghoust <tabish169000@gmail.com>
2025-07-13 06:38:36 -1200

    master basic done

---
+++ src/api/gellery/controllers/gellery.ts
@@ -0,0 +1,7 @@
+/**
+ * gellery controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::gellery.gellery');
---
+++ src/api/gellery/routes/gellery.ts
@@ -0,0 +1,7 @@
+/**
+ * gellery router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::gellery.gellery');
