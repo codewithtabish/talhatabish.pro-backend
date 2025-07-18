0c4ee410838ef8357f3be1eae24dc94df6b54d30
coderghoust <tabish169000@gmail.com>
2025-07-17 21:45:55 -1200

    added public dir

--- src/api/gellery/controllers/gellery.ts
+++ src/api/gellery/controllers/gellery.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::gellery.gellery');
+export default factories.createCoreController('api::gellery.gellery')
